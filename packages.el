573f342449c118928cb14d5c3eacb45a868f4567
Alex Ragone <alex.ragone@ezyvet.com>
2018-11-20 22:16:49 +1300

    first commit

---
+++ packages.el
@@ -0,0 +1,27 @@
+;;; packages.el --- description -*- lexical-binding: t; no-byte-compile: t; -*-
+
+(package! gruvbox-theme)
+(package! org-present)
+(package! request)
+(package! atomic-chrome)
+(package! org-clock-convenience)
+(package! js-doc)
+(package! sx)
+(package! phpcbf)
+(package! php-cs-fixer)
+(package! exec-path-from-shell)
+(package! tldr)
+(package! ox-clip)
+(package! ov)
+(package! deft)
+(package! org-jira)
+(package! vlf)
+(package! hackernews)
+(package! paradox)
+(package! org-mime)
+(package! vagrant-tramp)
+(package! gif-screencast)
+
+(provide 'packages)
+
+;;; packages.el ends here
